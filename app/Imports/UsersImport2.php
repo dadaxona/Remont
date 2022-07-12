20428422b7d60f13247017e8ae9d49e274d779ba
dadaxona <abdullaevdadaxon337@gmail.com>
2022-07-12 17:59:03 +0500

    sss

--- app/Imports/UsersImport2.php
+++ app/Imports/UsersImport2.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Ichkitavar;
+use App\Models\Updatetavr;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
@@ -12,7 +13,7 @@ class UsersImport2 implements ToCollection
     {
         foreach ($rows as $row) 
         {
-            Ichkitavar::create([
+           $data = Ichkitavar::create([
                 'tavar_id'=>$row[1],
                 'adress'=>$row[2],
                 'tavar2_id'=>$row[3],
@@ -25,6 +26,19 @@ class UsersImport2 implements ToCollection
                 'kurs'=>$row[10],
                 'kurs2'=>$row[11],
                 'kod'=>$row[12]
+            ]);
+
+            Updatetavr::create([
+                'tavar_id'=>$row[1],
+                'ichkitavar_id'=>$data->id,
+                'adress'=>$row[2],
+                'tavar2_id'=>$row[3],
+                'raqam'=>$row[5],
+                'hajm'=>$row[6],
+                'summa'=>$row[7],
+                'summa2'=>$row[8],
+                'summa3'=>$row[9],
+                'kod'=>$row[12],
             ]);
         }
     }
